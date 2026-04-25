813464864e2ebf06e69c7162db534ca535ffaf13
Rayssa Neder <rayssafreitas13@gmail.com>
2026-04-25 19:43:09 -0300

    Criando a estrutura base de UseCase

--- application/src/main/java/com/fullcycle/admin/catalogo/application/NullaryUseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/NullaryUseCase.java
@@ -1,8 +1,6 @@
 package com.fullcycle.admin.catalogo.application;
 
-import com.fullcycle.admin.catalogo.domain.category.Category;
+public abstract class NullaryUseCase<OUT> {
 
-public abstract class UseCase<IN, OUT> {
-
-    public abstract OUT execute(IN anIn);
+    public abstract OUT execute();
 }

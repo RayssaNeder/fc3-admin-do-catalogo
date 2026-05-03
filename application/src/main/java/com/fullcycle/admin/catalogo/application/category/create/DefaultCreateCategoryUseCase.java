34ab25befd0a6235350124504eb14dbdedef0384
Rayssa Neder <rayssafreitas13@gmail.com>
2026-05-02 22:41:38 -0300

    Impl notification pattern como um validation handler

--- application/src/main/java/com/fullcycle/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/create/DefaultCreateCategoryUseCase.java
@@ -2,6 +2,7 @@ package com.fullcycle.admin.catalogo.application.category.create;
 
 import com.fullcycle.admin.catalogo.domain.category.Category;
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.validation.handler.Notification;
 import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 
 public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
@@ -19,8 +20,15 @@ public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
        var aDescription = aComand.Description();
        var isActive = aComand.isActive();
 
+        final var notification = Notification.create();
+
         final var aCategory = Category.newCategory(aName, aDescription, isActive);
-        aCategory.validate(new ThrowsValidationHandler());
+        aCategory.validate(notification);
+
+        if(notification.hasError()){
+            //fazer alguma coisa
+        }
+
         return CreateCategoryOutput.from(this.categoryGateway.create(aCategory));
     }
 }
--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -7,8 +7,6 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(new UseCase().execute());
-
 
 
     }
